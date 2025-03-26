0699a26fc9ae5e118a56c470dba3484c17aae6cb
cargo9 <168007770+cargo9@users.noreply.github.com>
2025-03-26 19:06:05 +0200

    .

---
+++ script.js
@@ -0,0 +1,19 @@
+let games = ["Grand Theft Auto V", "My Summer Car", "Cuphead"];
+console.log(games[0]);
+console.log(games[2]);
+
+
+let animals = ["кіт", "слон", "папуга"];
+console.log(animals);
+
+
+let drinks = ["pepsi", "fanta"];
+drinks.push("cola");
+
+
+let colors = ["red", "green", "blue"];
+colors.shift("yellow");
+
+
+let lessons = ["укр мова", "математика", "історія", "біологія"];
+console.log(lessons.length);
